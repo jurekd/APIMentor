2fd84886f9469050fa56ef058f93dbbd9eba8dde
Jerzy Duda <jurekd@gmail.com>
2019-03-31 17:27:46 +0200

    Dodaj pliki projektów.

---
+++ WebApplication1/FoodRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+  public class FoodRecord
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
